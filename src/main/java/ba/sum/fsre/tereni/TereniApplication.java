a5863bfdc0ee47cff31ee29012caa4a0150db228
LeaLela <lea.rajic44@gmail.com>
2024-01-24 20:41:26 +0100

    Lea

---
+++ src/main/java/ba/sum/fsre/tereni/TereniApplication.java
@@ -0,0 +1,13 @@
+package ba.sum.fsre.tereni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TereniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TereniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ba/sum/fsre/tereni/TereniApplicationTests.java
@@ -0,0 +1,13 @@
+package ba.sum.fsre.tereni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TereniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
