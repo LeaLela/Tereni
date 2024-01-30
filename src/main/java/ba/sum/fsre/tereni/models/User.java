db85047cc95790917a4259559b2dd673e5791fe3
klarasarac <klarasarac55@gmail.com>
2024-01-30 13:26:23 +0100

    Klara

--- src/main/java/ba/sum/fsre/tereni/models/User.java
+++ src/main/java/ba/sum/fsre/tereni/models/User.java
@@ -1,25 +1,49 @@
 package ba.sum.fsre.tereni.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 import javax.annotation.processing.Generated;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class User {
     @Id
     @GeneratedValue
     private Long id;
-
+    @Size(min = 5, max = 20, message = "Polje ime mora biti izmđu 5 i 20 znakova.")
+    @NotBlank(message = "Polje ime je obvezno")
     String ime;
+
+    @Size(min = 5, max = 20, message = "Polje ime mora biti izmđu 5 i 20 znakova.")
+    @NotBlank(message = "Polje prezime je obvezno")
     String prezime;
+
+    @NotBlank(message = "Polje email je obvezno")
+    @Email(message = "Email adresa mora biti ispravnog formata.")
     String email;
 
-    public User(Long id,String ime, String prezime,String email){
-        this.id=id;
-        this.ime=ime;
-        this.prezime=prezime;
-        this.email=email;
+    @NotBlank(message = "Molimo unesite lozinku")
+    String lozinka;
+
+    @NotBlank(message = "Molimo ponovite lozinku")
+    @Transient
+    String potvrdaLozinke;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Enumerated(EnumType.STRING)
+    Set<Role> roles = new HashSet<>();
+    public User(Long id, String ime, String prezime, String email, String lozinka, String potvrdaLozinke) {
+        this.id = id;
+        this.ime = ime;
+        this.prezime = prezime;
+        this.email = email;
+        this.potvrdaLozinke = potvrdaLozinke;
+        roles.add(Role.STUDENT);
     }
 
     public User() {
@@ -56,5 +80,39 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getLozinka() {
+        return lozinka;
+    }
+
+    public void setLozinka(String lozinka) {
+        this.lozinka = lozinka;
+    }
+
+    public String getPotvrdaLozinke() {
+        return potvrdaLozinke;
+    }
+
+    public void setPotvrdaLozinke(String potvrdaLozinke) {
+        this.potvrdaLozinke = potvrdaLozinke;
+    }
+
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    @AssertTrue(message = "Lozinke se moraju podudarati")
+    public boolean isPasswordsEqual() {
+        try {
+            return this.lozinka.equals(this.potvrdaLozinke);
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
