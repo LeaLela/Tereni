7c73ec05faeca3be66113951e1273848b3fb5fe1
Ante2002 <antegudelj2002@gmail.com>
2024-01-31 12:39:16 +0100

    Ante

---
+++ src/main/java/ba/sum/fsre/tereni/models/UserDetails.java
@@ -0,0 +1,64 @@
+package ba.sum.fsre.tereni.models;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+public class UserDetails implements org.springframework.security.core.userdetails.UserDetails {
+    private User user;
+
+    public UserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getLozinka();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public String getFullName() {
+        return user.getIme() + " " + user.getPrezime();
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.name()))
+                .collect(Collectors.toList());
+    }
+}
