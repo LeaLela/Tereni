079f5d76ed916e04df9b765f716302d6d7d8a124
LeaLela <lea.rajic44@gmail.com>
2024-02-01 12:13:19 +0100

    Lea

--- src/main/java/ba/sum/fsre/tereni/repositories/ReservationRepository.java
+++ src/main/java/ba/sum/fsre/tereni/repositories/ReservationRepository.java
@@ -1,5 +1,12 @@
 package ba.sum.fsre.tereni.repositories;
 import ba.sum.fsre.tereni.models.Reservation;
+import ba.sum.fsre.tereni.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.util.List;
+
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+    @Query("SELECT r FROM Reservation r WHERE r.user = :user")
+    List<Reservation> findByUser(@Param("user")User user);
 }
