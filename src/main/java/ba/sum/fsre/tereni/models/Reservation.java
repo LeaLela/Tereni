830e537f6a7c29901c4a37863bff7931c5ff26c9
klarasarac <klarasarac55@gmail.com>
2024-02-01 11:48:29 +0100

    Klara

---
+++ src/main/java/ba/sum/fsre/tereni/models/Reservation.java
@@ -0,0 +1,63 @@
+package ba.sum.fsre.tereni.models;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime startTime;
+
+    private LocalDateTime endTime;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Reservation(Long id, LocalDateTime startTime, LocalDateTime endTime, User user) {
+        this.id = id;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.user = user;
+    }
+
+    public Reservation() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
