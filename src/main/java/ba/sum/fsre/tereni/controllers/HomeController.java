6fdf12d48c6a9af32e31aa90323b8de412347d05
klarasarac <klarasarac55@gmail.com>
2024-02-03 12:13:13 +0100

    Klara

--- src/main/java/ba/sum/fsre/tereni/controllers/HomeController.java
+++ src/main/java/ba/sum/fsre/tereni/controllers/HomeController.java
@@ -3,7 +3,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
-    @GetMapping("/")
+    @GetMapping("/home")
     public String home() {
         return "home";
     }
