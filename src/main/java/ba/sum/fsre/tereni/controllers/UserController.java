cf744627c64d1e11c2db22d63355cad5e0f03d89
LeaLela <lea.rajic44@gmail.com>
2024-02-05 20:52:20 +0100

    Lea

--- src/main/java/ba/sum/fsre/tereni/controllers/UserController.java
+++ src/main/java/ba/sum/fsre/tereni/controllers/UserController.java
@@ -36,7 +36,7 @@ public class UserController {
         model.addAttribute("role", role);
         model.addAttribute("userDetails", userDetails);
         model.addAttribute("activeLink", "Korisnici");
-        return "/index";
+        return "templates/index";
     }
     @GetMapping("/users")
     public String listUsers (Model model){
